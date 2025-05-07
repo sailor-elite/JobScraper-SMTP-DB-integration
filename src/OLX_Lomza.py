e904c66ecdf2f952d37f0ea152ae7a83dfded654
qq <jakub.j.bednarczyk@gmail.com>
2025-05-07 22:22:41 +0200

    fixed bug with mixing indexes and not showing proper location

--- src/OLX_Lomza.py
+++ src/OLX_Lomza.py
@@ -3,7 +3,10 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 import locale
+
+
 locale.setlocale(locale.LC_TIME, 'pl_PL.UTF-8')
+
 
 
 months_map = {
@@ -31,39 +34,61 @@ class OLX_Scrapper:
                     max_number = raw_numbers
 
             jobs = []
-            print(max_number)
+            #print(max_number)
             for i in range(0,max_number):
                 new_URL = self.URL + f"?page={i}"
                 job_response = requests.get(new_URL)
                 job_response.raise_for_status()
                 soup = BeautifulSoup(job_response.text, "html.parser")
-                if "css-1rpfcm7" in soup.find_all("div", class_="css-1ib10r7"):
-                    job_names = soup.find_all("a", class_="css-13gxtrp")
-                    job_employees = soup.find_all("p", class_="css-1rpfcm7")
-                elif "css-1rpfcm7" not in soup.find_all("div", class_="css-1ib10r7"):
-                    job_names = soup.find_all("a", class_="css-13gxtrp")
-                    job_employees = soup.find_all("a", class_="css-13gxtrp")
-                job_dates = soup.find_all("p", class_="css-996jis")
-                print(new_URL)
+                divs = soup.find_all("div", class_="css-l9drzq")
+                print ("site: ", i)
+                temp_count = 0
+                job_names = []
+                job_employees = []
+                job_dates = []
+                for div in divs:
+                    temp_count += 1
+                    print(f"Processing div {temp_count}: {div}")
+                    job_name = div.find("a", class_="css-13gxtrp")
+                    job_employee = div.find("p", class_="css-1rpfcm7")
+                    job_date = div.find("p", class_="css-996jis")
+                    if job_name:
+                        job_names.append(job_name)
+                        print(f"Found name in div {temp_count}: {job_name.get_text(strip=True)}")
+                    else:
+                        print(f"Job name not found in div {temp_count}")
+                        continue
+                    if job_employee:
+                        job_employees.append(job_employee)
+                        print(f"Found employee in div {temp_count}: {job_employee.get_text(strip=True)}")
+                    else:
+                        print(f"Employee not found in div {temp_count}, using job name as fallback")
+                        job_employees.append(job_name)
+                    if job_date:
+                        job_dates.append(job_date)
+                    else:
+                        print(f"Date not found in div {temp_count}")
+                        job_dates.append(None)
+
                 for name, place, date in zip(job_names,job_employees, job_dates):
                     job_title = name.get_text(strip=True)
                     job_location = place.get_text(strip=True)
 
                     job_date = date.get_text(strip=True).replace("\xa0", " ").strip()
-                    print(job_date)
+                    #print(job_date)
                     if "Dzisiaj" in job_date:
                         job_date = job_date.replace(job_date, datetime.today().strftime("%d.%m.%Y"))
                     elif "Odświeżono dnia" in job_date:
                         job_date = job_date.replace("Odświeżono dnia ", "")
                     if "Dzisiaj" not in job_date:
                         for month_name, month_num in months_map.items():
-                            print(job_date, " ", month_name)
+                            #print(job_date, " ", month_name)
                             if month_name in job_date:
                                 job_date = job_date.replace(month_name, month_num).replace(" ", "")
                                 try:
                                     job_date = datetime.strptime(job_date, "%d-%m-%Y").strftime("%d.%m.%Y")
                                 except ValueError as e:
-                                    print(f"Błąd parsowania: {e} dla daty: {job_date}")
+                                    print(f"parsing error: {e} date: {job_date}")
                                 break
                     jobs.append({"name": job_title, "date": job_date, "location": job_location, "source": "OLX"})
             return jobs
@@ -74,4 +99,4 @@ class OLX_Scrapper:
 
 olx = OLX_Scrapper()
 old = olx.fetch_jobs()
-print(old)
+#print(old)
